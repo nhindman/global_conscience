4a67c830a1e01c22ea00e7bffdbe416e5d3844a5
iwoodruff <isaac.woodruff@gmail.com>
2014-01-13 14:25:13 -0500

    rails default folders created, database established, rspec initiated. no tests yet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlobalConscience::Application.config.secret_token = '16974ffc29f98801ccff3eea0db8ef672c87ea9c7eb922c02c1e06b12868b5a681ed97128f73839ef2a818629ff83eae578421b5b39d6c0cedaec90203ea4d12'
