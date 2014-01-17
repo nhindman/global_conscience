69e38152127681d11c8e59b1d5fc8243d6ae3107
iwoodruff <isaac.woodruff@gmail.com>
2014-01-17 05:16:44 -0500

    changes?

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,6 @@ class PostsController < ApplicationController
     body = params[:body]
     country = params[:country]
     link= country.gsub(" ", "%20")
-    warning_id = params[:warning_id]  
     Comment.create(title: title, body: body, country: country, username: current_user.name)
     redirect_to "/country/display?country=#{link}"
   end
