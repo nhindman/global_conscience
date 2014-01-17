09c83776977d2d985f7f49b9ac5c63ea1dda5233
iwoodruff <isaac.woodruff@gmail.com>
2014-01-17 01:29:54 -0500

    fixed post methods. 1:30am. please work

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(:version => 20140116233653) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
-    t.integer  "country_id"
-    t.integer  "user_id"
+    t.integer  "warning_id"
+    t.string   "username"
     t.string   "title"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(:version => 20140116233653) do
   end
 
   create_table "users", :force => true do |t|
+    t.string   "name"
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
