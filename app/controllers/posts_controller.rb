ddea157534506e055f83a873085768525358d88d
iwoodruff <isaac.woodruff@gmail.com>
2014-01-16 22:14:16 -0500

    fixed posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,8 @@ class PostsController < ApplicationController
   def create do
     title = params[:title]
     body = params[:body]
-    id = params[]
-    Comment.create(title: title, body: body, warning_id: 5, user_id: User.id)
+    warning_id = params[:warning_id]
+    Comment.create(title: title, body: body, warning_id: warning_id, user_id: User.id)
     redirect_to 'country/display'
   end
 
